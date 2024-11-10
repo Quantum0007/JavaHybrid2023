1ec38a90d47c7adc9813937960f423fc1bfd1c37
karthik <karthik@gmail.com>
2024-11-10 20:58:38 +0530

    first.java

---
+++ first.java
@@ -0,0 +1,10 @@
+public class Greeting {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in;
+		System.put.print("hello world)";
+		String name = scanner.nextLine();
+
+		System.out.println("hellow, " + name +);
+		scnner.close();
+	}
+}
